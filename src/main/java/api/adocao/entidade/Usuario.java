cfdc19e7d35c0af82cfa88c5e2620395259594a3
João Danilo <jddiedrich@gmail.com>
2021-09-27 15:45:52 -0300

    Adicionada configuração do Banco de Dados

---
+++ src/main/java/api/adocao/entidade/Usuario.java
@@ -0,0 +1,38 @@
+package api.adocao.entidades;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String telefone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ src/main/java/api/adocao/repositorio/UsuarioRepository.java
@@ -0,0 +1,2 @@
+package api.adocao.repositorio;public interface UsuarioRepository {
+}
