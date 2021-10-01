0ee1dc66ef66e842ae5f0879539b6728cb3524d6
João Danilo <jddiedrich@gmail.com>
2021-10-01 09:04:17 -0300

    mudanças na tabela instituicao

--- src/main/java/api/adocao/entidade/Instituicao.java
+++ src/main/java/api/adocao/entidade/Instituicao.java
@@ -3,10 +3,7 @@ package api.adocao.entidade;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
@@ -14,9 +11,10 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 @Entity
-@Table(name = "entidade")
+@Table(name = "instituicao")
 @Getter
 @Setter
 @ToString
@@ -24,22 +22,13 @@ import java.util.Objects;
 public class Instituicao {
 
     @Id
-    private String cnpj;
-    @NotNull @NotEmpty @Min(5) @Max(255)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
-    @NotNull @NotEmpty @Min(5) @Max(255)
     private String endereco;
-
+    private String telefone;
     @OneToMany(mappedBy = "instituicao")
     @ToString.Exclude
     private List<Animal> animais = new ArrayList<>();
-
-    public void setAnimais(List<Animal> animais) {
-        this.animais = animais;
-    }
-
-    public List<Animal> getAnimais() {
-        return animais;
-    }
 
 }
