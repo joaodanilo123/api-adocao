96eaabe4f827254678ec5b99868737f75b8369c5
João Danilo <jddiedrich@gmail.com>
2021-10-01 09:11:36 -0300

    imports organizados

--- src/main/java/api/adocao/entidade/Instituicao.java
+++ src/main/java/api/adocao/entidade/Instituicao.java
@@ -1,17 +1,9 @@
 package api.adocao.entidade;
 
 import lombok.*;
-import org.hibernate.Hibernate;
-
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 @Entity
 @Table(name = "instituicao")
