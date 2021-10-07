edf98fbe0816d3c17323858ed595d39ca53590a0
Scryter <60343264+Scryter@users.noreply.github.com>
2021-10-07 14:51:39 -0300

    finalização das entidades Inventário

---
+++ src/main/java/api/adocao/controller/dto/InventarioDTO.java
@@ -0,0 +1,9 @@
+package api.adocao.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class InventarioDTO {
+    private Long id;
+    private String item;
+}
---
+++ src/main/java/api/adocao/controller/form/InventarioForm.java
@@ -0,0 +1,20 @@
+package api.adocao.controller.form;
+
+import api.adocao.entidade.Inventario;
+import api.adocao.repositorio.InventarioRepository;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class InventarioForm {
+    @NotNull
+    private String item;
+
+    public Inventario atualizar(Long id, InventarioRepository repository)
+    {
+        Inventario inventario = repository.getById(id);
+        inventario.setItem(this.item);
+        return inventario;
+    }
+}
