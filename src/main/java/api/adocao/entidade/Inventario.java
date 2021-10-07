4f36e5e756a38df8dc7d34a6bd5e24881156138d
Scryter <60343264+Scryter@users.noreply.github.com>
2021-10-07 12:22:39 -0300

    Update Inventario.java

--- src/main/java/api/adocao/entidade/Inventario.java
+++ src/main/java/api/adocao/entidade/Inventario.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @RestController
@@ -17,5 +18,7 @@ public class Inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String item;
 
 }
