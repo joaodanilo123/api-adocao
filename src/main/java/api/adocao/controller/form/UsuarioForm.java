72d5dc1d603cef762e008fdc8f9b321d477e479c
João Danilo <jddiedrich@gmail.com>
2021-10-11 08:33:43 -0300

    validacao de email e cpf em usuario form

--- src/main/java/api/adocao/controller/form/UsuarioForm.java
+++ src/main/java/api/adocao/controller/form/UsuarioForm.java
@@ -3,7 +3,9 @@ package api.adocao.controller.form;
 import api.adocao.entidade.Perfil;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -13,12 +15,14 @@ public class UsuarioForm {
     private String nome;
     @NotNull
     @Length(min = 3, max = 255)
+    @Email
     private String email;
     @NotNull
     @Length(min = 8)
     private String senha;
     @NotNull
     @Length(min = 11, max = 11)
+    @CPF
     private String cpf;
     @NotNull
     @Length(min = 3, max = 255)
