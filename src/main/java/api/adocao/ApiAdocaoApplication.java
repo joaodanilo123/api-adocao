943201e3bf3d0377798091a2d456954d3e727d1d
Scryter <60343264+Scryter@users.noreply.github.com>
2021-10-14 22:20:27 -0300

    suégue

--- src/main/java/api/adocao/ApiAdocaoApplication.java
+++ src/main/java/api/adocao/ApiAdocaoApplication.java
@@ -2,8 +2,10 @@ package api.adocao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiAdocaoApplication {
 
 	public static void main(String[] args) {
